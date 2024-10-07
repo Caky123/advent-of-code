aa611aba502fcb56e4c32aa1b15f8b64ab175e23
Jan Cakl <cakl.jan@gmail.com>
2024-10-07 13:53:42 +0200

    enumerate for count of loops

--- 01_2.py
+++ 01_2.py
@@ -3,11 +3,11 @@ from util import read_file_in_chunks as read_file
 result = 0
 index = 0
 
-for chunk in read_file('data/day01/input.txt', 1):
+for count, chunk in enumerate(read_file('data/day01/input.txt', 1), start = 1):
     result += chunk.count("(") - chunk.count(")")
-    index += 1
 
     if result == -1:
+        index = count
         break
 
 print(f"Result is <{index}>")
