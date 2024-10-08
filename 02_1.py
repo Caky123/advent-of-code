b4ebfdaa493db062c78a24defac3fea7311d6569
Jan Cakl <cakl.jan@gmail.com>
2024-10-08 10:53:44 +0200

    day 2

---
+++ 02_1.py
@@ -0,0 +1,28 @@
+from util import read_file_by_line as read_file
+
+class Present:
+    def __init__(self, data):
+
+        try:
+            self.l = int(data[0])
+            self.w = int(data[1])
+            self.h = int(data[2])
+            self.surface_area = 0
+            self.calc_dimension()
+        except Exception as e:
+            print(e)
+            exit(0)
+    
+    def calc_dimension(self):
+        lw = self.l*self.w
+        wh = self.w*self.h
+        hl = self.h*self.l
+
+        self.surface_area = 2*lw + 2*wh + 2*hl + min(lw, wh, hl)
+
+total = 0
+for line in read_file('data/day02/input.txt'):
+    present = Present(line.split("x"))
+    total += present.surface_area
+
+print(total)
--- util.py
+++ util.py
@@ -3,3 +3,8 @@ def read_file_in_chunks(file_path, chunk_size=1024):
     with open(file_path, 'r') as file:
         while chunk := file.read(chunk_size):
             yield chunk
+
+def read_file_by_line(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            yield line.strip()
